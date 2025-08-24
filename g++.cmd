f54591dd89ddaa13fe66ca5556481f97acc18294
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-25 00:04:52 +0300

    Fix scripts.

--- g++.cmd
+++ g++.cmd
@@ -1,1 +1,4 @@
-@"%~dp0tools\pour_wrapper_windows.exe" --run foreign-linux "usr/bin/g++" %* && exit /B 0 || exit /B 1
+@echo off
+setlocal
+cd "%~dp0" || exit /B 1
+"%~dp0tools\pour_wrapper_windows.exe" --run foreign-linux "usr/bin/g++" %* && exit /B 0 || exit /B 1
--- gcc.cmd
+++ gcc.cmd
@@ -1,1 +1,4 @@
-@"%~dp0tools\pour_wrapper_windows.exe" --run foreign-linux "usr/bin/gcc" %* && exit /B 0 || exit /B 1
+@echo off
+setlocal
+cd "%~dp0" || exit /B 1
+"%~dp0tools\pour_wrapper_windows.exe" --run foreign-linux "usr/bin/gcc" %* && exit /B 0 || exit /B 1
