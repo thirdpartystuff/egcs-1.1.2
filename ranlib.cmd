0bcbae347aa0a4e017f0a008afdbc3088fc5d330
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-25 19:08:39 +0300

    Fix command invocation with flinux.

--- ranlib.cmd
+++ ranlib.cmd
@@ -1,4 +1,1 @@
-@echo off
-setlocal
-cd "%~dp0" || exit /B 1
-"%~dp0tools\pour_wrapper_windows.exe" --run foreign-linux "usr/bin/ranlib" %* && exit /B 0 || exit /B 1
+@"%~dp0tools\pour_wrapper_windows.exe" --chdir "%~dp0." --run foreign-linux "/usr/bin/ranlib" %* && exit /B 0 || exit /B 1
